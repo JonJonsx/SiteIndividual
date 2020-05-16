1bda543813f983a4ab60caf794ab2db7d48ab91f
larissaSousa <larissa.sousa@bandtec.com.br>
2020-05-16 19:01:06 -0300

    função que pega poke movida para a página index

--- js/cards.js
+++ js/cards.js
@@ -1,5 +1,4 @@
 import pickColor from './getColor.js';
-import Pokemon from './dataPoke.js';
 
 function buildCard(pokemon){
     document.getElementById("boxCard").innerHTML += 
@@ -15,31 +14,5 @@ function buildCard(pokemon){
         </div>
     </div>`;
 }
-
 // ${data.types.length > 1 ? '<label class="types">'+data.types[1].type.name+'</label>' : ''}
-
-const datapoke = () =>{
-    for(var i = 1; i <= 20; i ++){
-        const numero = i;
-        
-        Pokemon.getPokemon(i).then(data => {
-            const pokemon = new Pokemon(data.id,
-                data.name,
-                data.sprites.front_default,
-                data.height,
-                data.weight,
-                data.stats[5].base_stat,
-                data.stats[4].base_stat,
-                data.stats[3].base_stat,
-                data.stats[2].base_stat,
-                data.stats[1].base_stat,
-                data.stats[0].base_stat,
-                data.types[0].type.name
-                );
-                buildCard(pokemon);
-        });
-    }
-}
-window.addEventListener('load',function(){
-    datapoke();
-})
+export default buildCard;
