2dd177ed1879a71b56187c2422ea607bfbf9b743
JonJonsx <jonatan.souza@bandtec.com.br>
2020-06-09 21:53:31 -0300

    tela de loading adicionada

---
+++ js/loader.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+    setTimeout(() => {
+        const loader = document.querySelector('.loading');
+        loader.style.display = "none";
+    }, 500);
+})
