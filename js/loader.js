8d4ffd0f14acaaebc38fa9b14dc98259eaec617a
JonJonsx <jonatan.souza@bandtec.com.br>
2020-06-09 22:20:52 -0300

    configurando navBar para o mobile

--- js/loader.js
+++ js/loader.js
@@ -4,3 +4,15 @@ window.addEventListener('load', function () {
         loader.style.display = "none";
     }, 500);
 })
+
+var menu = document.getElementsByClassName('menu')[0];
+if (screen.width <= 780) {
+    menu.style.display = "none";
+}   
+function closeOpen() {
+    if(menu.style.display == 'block'){
+        menu.style.display = "none";
+    }else{
+        menu.style.display = "block";
+    }
+}
